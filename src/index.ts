80835cc55e75f8efc06773b3e54c2066523fd078
Ålex Acuña Viera <kyuumeitai@gmail.com>
2024-06-07 01:59:51 -0400

    swagger

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import express from "express";
-import compression from "compression";
-import { configDotenv } from "dotenv";
-import { listing, results, search, zoneDefinitions } from "./api/listElements";
+import express from 'express';
+import compression from 'compression';
+import { configDotenv } from 'dotenv';
+import { listing, results, search, zoneDefinitions } from './api/listElements';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger_output.json';
 
 const electionReader = express();
 electionReader.use(express.json());
 electionReader.use(compression());
 configDotenv();
 
-
 const PORT = process.env.PORT ?? '3333';
 
 electionReader.listen(PORT, () => {
-  console.log(`app running on port => ${PORT} 🐳`)
+  console.log(`app running on port => ${PORT} 🐳`);
 });
 
-electionReader.use('/api/def/',zoneDefinitions);
-electionReader.use('/api/check/',listing);
-electionReader.use('/api/result/',results);
-electionReader.use('/api/search/',search);
+electionReader.use('/api/def/', zoneDefinitions);
+electionReader.use('/api/check/', listing);
+electionReader.use('/api/result/', results);
+electionReader.use('/api/search/', search);
+
+electionReader.use('/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
